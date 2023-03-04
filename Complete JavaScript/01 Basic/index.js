7557ad4e017422479a3bd8be306037429c02f185
Ajay Sonere <ajaysonere786@gmail.com>
2023-03-04 17:46:38 +0530

    basic of js

---
+++ Complete JavaScript/01 Basic/index.js
@@ -0,0 +1,32 @@
+console.log("Hello World ");
+
+// types in number
+
+// 1. number
+console.log(10);
+console.log(10.2);
+console.log(-22);
+
+// string 
+console.log("String in JS By double quote");
+console.log('string in js By double by single quote');
+console.log(`string in js By backticks `);
+
+// boolean
+console.log(true);
+console.log(false);
+
+//  undefinded and null
+var a;
+console.log(a);
+console.log(null);
+
+// symbol  in js 
+console.log(Symbol('hello'));
+
+console.log(typeof a);
+
+// process
+process.stdout.write("hello ");
+process.stdout.write("world");
+
