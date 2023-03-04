9dfed2914ce5bdc9804840abaa656b32328d822a
Ajay Sonere <ajaysonere786@gmail.com>
2023-03-04 18:44:52 +0530

    variable and operate

---
+++ Complete JavaScript/01 Basic/variable.js
@@ -0,0 +1,29 @@
+// there are 3 way to initilize the variable 
+
+let x = 10;
+var b = 20;
+const c = 30;
+console.log(x);
+console.log(b);
+console.log(c);
+
+
+// operator in Js
+
+let a = 10;
+
+
+// arithmatic opetator 
+console.log(a + 2);
+console.log(a - 2);
+console.log(a * 2);
+console.log(a / 2);
+console.log(a % 2);
+
+// logical operator
+console.log((4 > 2) && (2 < 4));
+console.log((4> 3) || (4> 5));
+console.log(4 && 0);
+console.log(0 && 4);
+console.log(4 || 0);
+console.log(0 || 4);
