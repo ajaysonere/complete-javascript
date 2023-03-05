a5f8a97f90a3fa1ec3534c0cc7ac3cb0d260f39f
Ajay Sonere <ajaysonere786@gmail.com>
2023-03-05 09:54:30 +0530

    loop

--- Complete JavaScript/02 Loops/while.js
+++ Complete JavaScript/02 Loops/while.js
@@ -1,10 +1,31 @@
 
 // while loop 
-let i = 1;
-while(i<10){
-    console.log(i);
-    i++;
+// let i = 1;
+// while(i<10){
+//     console.log(i);
+//     i++;
+// }
+
+
+// // for loop 
+
+// for(let i=0 ; i<10; i++){
+//     console.log(i);
+// }
+
+// let x = 1;
+// do{
+//     console.log(x);
+//     x++;
+// }while(x<10);
+
+for(let i=0; i<5; i++){
+    let str = "";
+    for(let j=0; true; j++){
+        str += "*"; 
+        if(i==j){
+            break;
+        }
+    }
+    console.log(str);
 }
-
-
-// for loop 
