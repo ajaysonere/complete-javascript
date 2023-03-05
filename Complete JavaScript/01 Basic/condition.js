1b76cb0178cb5f6d80d28da44003665681a72b13
Ajay Sonere <ajaysonere786@gmail.com>
2023-03-05 09:02:22 +0530

    loops

--- Complete JavaScript/01 Basic/condition.js
+++ Complete JavaScript/01 Basic/condition.js
@@ -7,7 +7,7 @@ if(10 < 20){
 }
 
 // if -else condition
-if(102%2 === 0){
+if(10%2 === 0){
     console.log("Even Number ");
 }else{
     console.log("Odd Number ");
---
+++ Complete JavaScript/02 Loops/while.js
@@ -0,0 +1,10 @@
+
+// while loop 
+let i = 1;
+while(i<10){
+    console.log(i);
+    i++;
+}
+
+
+// for loop 
