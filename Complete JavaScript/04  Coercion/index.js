5ebf29a22ef2ea3377e9b2481a5faa2050e1d5da
Ajay Sonere <ajaysonere786@gmail.com>
2023-03-07 14:44:05 +0530

    coercion

--- Complete JavaScript/04  Coercion/index.js
+++ Complete JavaScript/04  Coercion/index.js
@@ -1,8 +1,14 @@
 // coercion in js
 
-console.log(1 + "2");
-console.log(true - 4);
-console.log(1 - 'abc');
-console.log('a' - true);
-console.log("as"  - "sa");
-console.log(11 - {"a": 10 , valueOf(){return this.a}});
+// console.log(1 + "2");
+// console.log(true - 4);
+// console.log(1 - 'abc');
+// console.log('a' - true);
+// console.log("as"  - "sa");
+// console.log(11 - {"a": 10 , valueOf(){return this.a}});
+
+console.log(11 - {"a":5 , valueOf(){return this.a}});
+
+console.log(33 + {a:20});
+console.log("33" - { a: 30 });
+console.log(33 - { a: 30 });
