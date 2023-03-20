ddb2e0d352e42930ff2cb3c982ec229b145b1f6f
Ajay Sonere <ajaysonere786@gmail.com>
2023-03-20 22:38:55 +0530

    Toboolean

--- Complete JavaScript/05 ToBooleanAndEquality/index.js
+++ Complete JavaScript/05 ToBooleanAndEquality/index.js
@@ -6,22 +6,26 @@ console.log({"a":10}+"Hello");
 console.log({}+"Hello");
 
 console.log(!0);
+console.log("Hello"+10);
 
 
 // to-boolean and quality operater 
 
 console.log(!(-4));
 
+console.log(!0);
+console.log(!1);
+console.log(!{});
 
-if(10 == "10"){
-    console.log("equal ");
-}else{
-    console.log("not equal");
-}
+// if(10 == "10"){
+//     console.log("equal ");
+// }else{
+//     console.log("not equal");
+// }
 
 
-if (10 === "10") {
-    console.log("equal ");
-} else {
-    console.log("not equal");
-}
+// if (10 === "10") {
+//     console.log("equal ");
+// } else {
+//     console.log("not equal");
+// }
