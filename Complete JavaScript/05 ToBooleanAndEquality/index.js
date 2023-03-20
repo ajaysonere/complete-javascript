004fb5588833ff664b5c75e00152d1c99fb58adb
Ajay Sonere <ajaysonere786@gmail.com>
2023-03-20 23:46:30 +0530

    equality operations

--- Complete JavaScript/05 ToBooleanAndEquality/index.js
+++ Complete JavaScript/05 ToBooleanAndEquality/index.js
@@ -1,4 +1,13 @@
 
+console.log(NaN === NaN);
+
+console.log(NaN === 10);
+
+console.log(10 === 10);
+
+console.log("AJAY" === "AJAY");
+console.log("AJAY" === "Ajay");
+
 console.log({"a":10});
 
 console.log({"a":10}+"Hello");
@@ -16,6 +25,8 @@ console.log(!(-4));
 console.log(!0);
 console.log(!1);
 console.log(!{});
+
+console.log(!console.log(0));
 
 // if(10 == "10"){
 //     console.log("equal ");
