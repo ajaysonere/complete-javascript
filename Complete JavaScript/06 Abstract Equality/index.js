eca0967ea60b5ef3a6b02100d20c704f69281cd2
Ajay Sonere <ajaysonere786@gmail.com>
2023-03-21 22:37:15 +0530

    abstact equality operator

--- Complete JavaScript/06 Abstract Equality/index.js
+++ Complete JavaScript/06 Abstract Equality/index.js
@@ -1,1 +1,16 @@
 // abstract equality operator
+console.log(null == undefined);
+console.log(null === undefined);
+console.log(10 == "10");
+console.log(10 === "10");
+console.log("152" == 152);
+console.log(true == 1);
+console.log(false == 1);
+
+console.log(10 == {valueOf(){return 10;}});
+
+let ans =0;
+for(let i=0;i<"5"; i++){
+    ans++;
+}
+console.log(ans);
