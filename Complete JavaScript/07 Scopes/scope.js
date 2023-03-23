f641d7052a902c63da0f0e11413d5c7899f84cb7
Ajay Sonere <ajaysonere786@gmail.com>
2023-03-23 21:56:55 +0530

    scopes

--- Complete JavaScript/07 Scopes/scope.js
+++ Complete JavaScript/07 Scopes/scope.js
@@ -5,5 +5,40 @@ console.log(x);
 
 function fun(){
     var y = 10;
+    console.log(y);
 }
-console.log(y);
+
+fun();
+
+
+// if(true){
+//     var a = 10;
+// }
+
+// if(false){
+//     var b = 20;
+// }
+
+// console.log(a);
+// console.log(b);
+
+
+// let 
+// {
+//     let x = 50;
+//     console.log(x);
+// }
+
+// console.log(x);
+
+var teacher = "Ajay";
+function fun(){
+    var teacher = "vijay";
+    teacherAssign = "A";
+    console.log(teacher);
+    console.log(teacherAssign);
+}
+console.log(teacher);
+fun();
+console.log(teacherAssign);
+
