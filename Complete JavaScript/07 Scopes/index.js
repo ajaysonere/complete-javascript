a2f061af4113cde940ff29bcf19bb13cb0a2b6fb
Ajay Sonere <ajaysonere786@gmail.com>
2023-03-23 19:00:35 +0530

    scopes

--- Complete JavaScript/07 Scopes/index.js
+++ Complete JavaScript/07 Scopes/index.js
@@ -1,1 +1,18 @@
 // scopes
+let name = "Ajay";
+
+function greet(){
+    console.log("Greeting from ",name);
+    var x = 20;
+    function test(){
+        console.log("test is ",x);
+    }
+    
+}
+
+function fun(){
+    console.log("Have Fun ! ", name);
+}
+
+fun1();
+fun2();
