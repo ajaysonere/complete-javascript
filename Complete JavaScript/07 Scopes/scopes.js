3bfdc2086bbf85f02708d592f3046650f30dc632
Ajay Sonere <ajaysonere786@gmail.com>
2023-03-24 15:52:11 +0530

    scopes contined

---
+++ Complete JavaScript/07 Scopes/scopes.js
@@ -0,0 +1,11 @@
+// scopes
+var teacher = "Ajay";
+function fun(){
+    var teacher = "Ajay";
+    techAssignment = "Ha";
+    console.log(teacher , techAssignment);
+}
+
+console.log(teacher);
+fun();
+console.log(techAssignment);
