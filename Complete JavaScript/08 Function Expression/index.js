83ad3d5ac30537aa0c71d05c0dad52bf07a9254d
Ajay Sonere <ajaysonere786@gmail.com>
2023-03-25 08:13:27 +0530

    function expression

--- Complete JavaScript/08 Function Expression/index.js
+++ Complete JavaScript/08 Function Expression/index.js
@@ -1,1 +1,13 @@
 // function expression
+function fun(){
+    console.log("fun is called ");
+}
+
+function gun(fn){
+   console.log("inside a gun");
+   fn();
+   console.log("leaving gun");
+}
+
+gun(fun);
+
