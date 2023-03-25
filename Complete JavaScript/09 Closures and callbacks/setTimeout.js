dbfab17c95fd36faa6356b64ecac8cc3a2b3e32c
Ajay Sonere <ajaysonere786@gmail.com>
2023-03-25 12:37:22 +0530

    closures

--- Complete JavaScript/09 Closures and callbacks/setTimeout.js
+++ Complete JavaScript/09 Closures and callbacks/setTimeout.js
@@ -1,1 +1,21 @@
 // setTimeout Function
+const id =  setTimeout(function (){
+    console.log("callback task 1");
+} , 10000);
+
+setTimeout(function(){
+    console.log("callback task 2");
+    clearTimeout(id);
+},5000);
+
+
+const uid = setInterval(function (){
+    console.log("Set Interval 1");
+} ,10000);
+
+setInterval(function (){
+    console.log("set interval 2");
+    clearInterval(uid); 
+},2000);
+
+
