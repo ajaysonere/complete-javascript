73e896898f55336db6cce300cbb3d83f55e9cace
Ajay Sonere <ajaysonere786@gmail.com>
2023-03-25 11:58:47 +0530

    setTimeout

--- Complete JavaScript/09 Closures and callbacks/index.js
+++ Complete JavaScript/09 Closures and callbacks/index.js
@@ -1,1 +1,19 @@
-sd
+function callback(x ,fun){
+    for( let i=0; i<x ;i++){
+        console.log(i);
+    }
+    fun();
+}
+
+callback(10,function(){
+     console.log("callback is called ");
+})
+
+console.log("New Function ");
+let arr = [1,2,3,4,5,6]; 
+let ans =  arr.map(function process(v,i){
+    console.log(v, i);
+    return v*v;
+});
+
+console.log(ans);
