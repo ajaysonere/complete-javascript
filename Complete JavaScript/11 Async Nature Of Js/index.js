2f938b900bdfa632f11e3a1f8583b7295bbe7467
Ajay Sonere <ajaysonere786@gmail.com>
2023-03-26 15:51:22 +0530

    async nature of js

--- Complete JavaScript/11 Async Nature Of Js/index.js
+++ Complete JavaScript/11 Async Nature Of Js/index.js
@@ -1,1 +1,17 @@
-// async natuer of js
+// sync natuer of js
+console.log("Start Execution By sync");
+for(let i=0; i<1000000000;i++){
+    // It will take some time
+}
+console.log("Stop Execution By sync ");
+
+console.log("Start Execution By async ");
+
+setTimeout(function(){
+    for (let i = 0; i < 1000000000; i++) {
+        // It will take some time
+    }
+},0);
+
+console.log("Stop Execution By async ")
+
