5ce748cd6d55d8dcc36ef36ef53cad4411804a56
Ajay Sonere <ajaysonere786@gmail.com>
2023-03-26 11:41:39 +0530

    promises and callbacks

--- Complete JavaScript/10 Promises and Callbacks/index.js
+++ Complete JavaScript/10 Promises and Callbacks/index.js
@@ -1,1 +1,10 @@
 // Promises And Callbacks
+function todo(task){
+    setTimeout(function (){
+        console.log(task ,"is Completed ");
+    }, 5000);
+}
+
+console.log("Assignment Start ");
+todo("assignment");
+console.log("Assignment end ");
