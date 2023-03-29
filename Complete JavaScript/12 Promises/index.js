6881e9279d1de4622927a23b64cd3e62f19a77e2
Ajay Sonere <ajaysonere786@gmail.com>
2023-03-29 22:53:59 +0530

    inversion of control

--- Complete JavaScript/12 Promises/index.js
+++ Complete JavaScript/12 Promises/index.js
@@ -1,5 +1,14 @@
 // Promises
-obj = {x:5}
-console.log(obj);
-obj.x = 10;
-console.log(obj);
+// obj = {x:5}
+// console.log(obj);
+// obj.x = 10;
+// console.log(obj);
+
+
+// inversion of control
+function fun(inputString , fn){
+    let arr = inputString.split(',');
+    for(let i=0; i<arr.length; i++){
+        fn(arr[i]);
+    }
+}
