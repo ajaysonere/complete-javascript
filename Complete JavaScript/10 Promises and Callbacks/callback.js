e39e07ed1d5f3354382dba803007471691e78091
Ajay Sonere <ajaysonere786@gmail.com>
2023-03-31 22:07:41 +0530

    callback hell

--- Complete JavaScript/10 Promises and Callbacks/callback.js
+++ Complete JavaScript/10 Promises and Callbacks/callback.js
@@ -16,6 +16,21 @@ function writeFile(data , fn){
     }, 4000);
 }
 
+function uploadFile(filename ,fn){
+    console.log("start uploading ",filename);
+    setTimeout(function process(){
+        console.log("file uploaded ");
+        let response = "succesfully";
+        fn(response);
+    }, 2000)
+}
+
 fetchFunction("www.google.com" , function process(response){
     console.log(response);
+    writeFile("My name is Ajay Sonere", function process(response) {
+        console.log("Data is writing in file ", response);
+        uploadFile("output.txt", function process(response) {
+            console.log("upload completed ", response);
+        });
+    });
 });
