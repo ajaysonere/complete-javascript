c12fa951b4d750893d05b7bb28e95cce5f91205a
Ajay Sonere <ajaysonere786@gmail.com>
2023-04-02 19:58:27 +0530

    promises

--- Complete JavaScript/10 Promises and Callbacks/promisesContinued.js
+++ Complete JavaScript/10 Promises and Callbacks/promisesContinued.js
@@ -11,7 +11,35 @@ function fetchData(url){
 
 function writeFile(data){
     return new Promise (function (resolve , reject){
-       console.log("writing on data ");
-       
+       console.log("writing in file " , data);
+       setTimeout(function process(){
+         let filename = "output.txt";
+         console.log("Writing is completed ");
+         resolve(filename);
+       },3000);
     });
 }
+
+function uploadFile(filename){
+    return new Promise(function(resolve , reject){
+       console.log("File uploding is started " ,filename);
+       setTimeout(function process(){
+        let uploadedOn = "aws"; 
+        console.log("File uploaded sucessfully ");
+        resolve(uploadedOn);
+       },4000);
+    });
+}
+
+let fetchDownload = fetchData("www.drive.com");
+fetchDownload.then(function process(value){
+    console.log("Download promise is  fulfilled " , value);
+    let getWritingData = writeFile(value);
+    getWritingData.then(function(value){
+        console.log("writing data fulfilled in file ", value);
+        let upload = uploadFile(value);
+        upload.then(function(value){
+            console.log("file is uploaded " , value);
+        });
+    });
+});
