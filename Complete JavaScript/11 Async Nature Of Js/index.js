7de33bcb20e1a91fb1567ce88f16a7955cd5cc75
Ajay Sonere <ajaysonere786@gmail.com>
2023-04-03 23:54:33 +0530

    async and await

--- Complete JavaScript/11 Async Nature Of Js/index.js
+++ Complete JavaScript/11 Async Nature Of Js/index.js
@@ -1,17 +1,56 @@
 // sync natuer of js
-console.log("Start Execution By sync");
-for(let i=0; i<1000000000;i++){
-    // It will take some time
+// console.log("Start Execution By sync");
+// for(let i=0; i<1000000000;i++){
+//     // It will take some time
+// }
+// console.log("Stop Execution By sync ");
+
+// console.log("Start Execution By async ");
+
+// setTimeout(function(){
+//     for (let i = 0; i < 1000000000; i++) {
+//         // It will take some time
+//     }
+// },0);
+
+// console.log("Stop Execution By async ");
+
+function fetchData(url) {
+    return new Promise(function (resolve, reject) {
+        console.log("Downloading start from ", url);
+        setTimeout(function process() {
+            console.log("Downloading is completed ");
+            let data = "dummy data";
+            resolve(data);
+        }, 5000);
+    });
 }
-console.log("Stop Execution By sync ");
 
-console.log("Start Execution By async ");
+function writeFile(data) {
+    return new Promise(function (resolve, reject) {
+        console.log("writing in file ", data);
+        setTimeout(function process() {
+            let filename = "output.txt";
+            console.log("Writing is completed ");
+            resolve(filename);
+        }, 3000);
+    });
+}
 
-setTimeout(function(){
-    for (let i = 0; i < 1000000000; i++) {
-        // It will take some time
-    }
-},0);
+function uploadFile(filename) {
+    return new Promise(function (resolve, reject) {
+        console.log("File uploding is started ", filename);
+        setTimeout(function process() {
+            let uploadedOn = "aws";
+            console.log("File uploaded sucessfully ");
+            resolve(uploadedOn);
+        }, 4000);
+    });
+}
 
-console.log("Stop Execution By async ");
+async function processing(){
+    let getData = await fetchDownload("www.ajaysonre.com");
+    let writtenData = await writeFile(getData);
+    let upload = await uploadFile(writtenData);
+}
 
