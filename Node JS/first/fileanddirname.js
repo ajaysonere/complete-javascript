39e3fbff1b1b5ac73cc69a373f8c4d05c40675f3
Ajay Sonere <ajaysonere786@gmail.com>
2023-04-10 09:10:24 +0530

    node

--- Node JS/first/fileanddirname.js
+++ Node JS/first/fileanddirname.js
@@ -1,3 +1,7 @@
 import path from "path";
 import url from "url";
 
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname  = path.dirname(__filename);
+console.log("File Path ", __filename);
+console.log("Dir path " , __dirname);
