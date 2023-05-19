9acf8fe5418ba258378303f45e592873a3ba02fe
Ajay Sonere <ajaysonere786@gmail.com>
2023-05-19 22:37:23 +0530

    express server

--- Node JS/first-express-project/index.js
+++ Node JS/first-express-project/index.js
@@ -1,1 +1,22 @@
 // basic server using the express
+
+import express from 'express';
+
+const app = express();
+const PORT = 4512;
+
+app.get('/' , (req , res )=>{
+    res.send("HELLO WORLD");
+})
+
+app.post("/",(req , res)=>{
+    res.send("HELLO WORLD FROM POST METHOD");
+})
+
+app.patch("/" , (req , res)=>{
+   res.send("HELLO WORLD FROM PATCH METHOD");
+})
+
+app.listen(PORT , ()=>{
+    console.log(`server is runnig at http://localhost:${PORT}`);
+})
