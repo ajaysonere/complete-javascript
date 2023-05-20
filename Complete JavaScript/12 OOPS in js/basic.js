753f37b85f6ca8d2e3d7dea65c1553bf6c70621a
Ajay Sonere <ajaysonere786@gmail.com>
2023-05-20 20:53:36 +0530

    oopes in js

---
+++ Complete JavaScript/12 OOPS in js/basic.js
@@ -0,0 +1,18 @@
+class Product{
+    constructor(name , price){
+        this.name = name;
+        this.price = price;
+    }
+    displayProduct(){
+
+    }
+    buyProduct(){
+
+    }
+}
+
+let Mobile = new Product("Redmi Note 7 Pro" , 15000);
+let Laptop = new Product("HP Ryzan 5 ", 50000);
+
+console.log(Mobile);
+console.log(Laptop);
