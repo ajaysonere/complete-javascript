d1d5980a04d48bc32f9b6b819a14e1212f2f74f3
Ajay Sonere <ajaysonere786@gmail.com>
2023-05-30 19:53:27 +0530

    api fetched succesfully

--- Node JS/Node Project/index.js
+++ Node JS/Node Project/index.js
@@ -1,11 +1,26 @@
-import http from 'http';
+import express from 'express';
+import axios from 'axios';
 
+const app = express();
 const PORT = 4500;
 
-const server = http.createServer((req , res)=>{
-   res.end("Server Stoped ");
+const API_KEY = "989a3e4e18624839987141158233005";
+
+
+app.get('/weather/:city' , (req , res)=>{
+    try {
+        const city = req.params.city;
+        if(city !== " "){
+            axios.get(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`).then(response =>{
+                const data = response.data;
+                res.send(data);
+            })
+        }
+    } catch (error) {
+        res.send("Error has occured ");
+    }
 });
 
-server.listen(PORT , ()=>{
-    console.log(`server is running at http://localhost:${PORT}`);
+app.listen(PORT , ()=>{
+    console.log(`Server is running at http://localhost:${PORT}`);
 })
