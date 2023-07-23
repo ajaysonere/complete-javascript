190f4bb61e29ff1e980d09213223b119e9739753
Ajay Sonere <ajaysonere786@gmail.com>
2023-07-23 15:15:33 +0530

    no

--- Node JS/Node Project/index.js
+++ Node JS/Node Project/index.js
@@ -7,11 +7,11 @@ const PORT = 4500;
 const API_KEY = "989a3e4e18624839987141158233005";
 
 
-app.get('/weather/:city' , (req , res)=>{
+app.get('/weather/:city' ,async (req , res)=>{
     try {
         const city = req.params.city;
         if(city !== " "){
-            axios.get(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`).then(response =>{
+            await axios.get(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`).then(response =>{
                 const data = response.data;
                 res.send(data);
             })
